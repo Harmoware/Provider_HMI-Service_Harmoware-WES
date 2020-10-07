fb7e15ead03cd74f7f9f94fe5e6f1a0382300fb2
Nobuo Kawaguchi <kawaguti@nagoya-u.jp>
2020-10-07 23:21:39 +0900

    update build ps1

--- build.ps1
+++ build.ps1
@@ -4,4 +4,4 @@ $sha1 = (git rev-parse HEAD).Trim()
 $gitver = (git describe --tag).Trim()
 $now = Get-Date -UFormat "%Y-%m-%d_%T"
 echo "Building go binary with GitInfo $gitver $now $sha1"
-go build  -ldflags "-X github.com/synerex/synerex_sxutil.Sha1Ver=$sha1 -X github.com/synerex/synerex_sxutil.BuildTime=$now -X github.com/synerex/synerex_sxutil.GitVer=$gitver" state-publish.go
+go build  -ldflags "-X github.com/synerex/synerex_sxutil.Sha1Ver=$sha1 -X github.com/synerex/synerex_sxutil.BuildTime=$now -X github.com/synerex/synerex_sxutil.GitVer=$gitver" hmi-service.go
